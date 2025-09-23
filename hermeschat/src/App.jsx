7ab05a4631988977a5ec9766bce91e520af55662
Victor <victorrodrigues288@gmail.com>
2025-09-22 22:07:45 -0300

    Configurações iniciais do projeto

---
+++ hermeschat/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hermes Chat</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
